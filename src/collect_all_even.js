892c249f63d2a53a564849d2643ddf21d6ad5e45
zhyingjia <965330394@qq.com>
2016-06-06 00:31:11 +0800

    first commit

---
+++ src/collect_all_even.js
@@ -0,0 +1,27 @@
+
+function collect_same_elements(collection_a, object_b) {
+	
+	var collection_same_elements = {value: []};
+
+	var k=0;
+
+
+	for( var i=0; i < collection_a.length; i++ ){
+		
+		for( var j=0; j < object_b.value.length; j++ ){
+		
+			if( collection_a[i].key == object_b.value[j] ){
+			
+				collection_same_elements.value[k++] = object_b.value[j];
+			
+			}
+		
+		}
+	
+	} 
+
+	return collection_same_elements;
+
+}
+
+module.exports = collect_same_elements;
